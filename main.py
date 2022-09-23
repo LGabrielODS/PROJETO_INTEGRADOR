c7a722fd468e9500ed2bc42d7c6cb2b9089e03e8
LGabrielODS <lucasgoliveira2@gmail.com>
2022-09-22 21:55:22 -0300

    CRIAÇÃO DE FUNÇÃO INSERIR

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #IMPORTAÇÃO DE BIBLIOTECAS
 
+from statistics import quantiles
 from pickle import FRAME
 from tkinter import*
 from tkinter import Tk, StringVar, ttk
@@ -10,6 +11,7 @@ from turtle import bgcolor, left, width
 from PIL import Image, ImageTk
 from tkcalendar import Calendar, DateEntry
 import tkinter.font as tkFont
+from view import *
 
 #CORES
 
@@ -46,6 +48,45 @@ framemeio.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
 framebaixo = Frame(janela, width=1050, height=300, bg=co1, relief="sunken")
 framebaixo.grid(row=2, column=0, pady=0, padx=1, sticky=NSEW)
 
+#CRIANDO FUNÇÕES
+global tree
+
+#função inserir
+def inserir():
+    global imagem,imagem_string,l_imagem
+    nome        =   e_nome.get()
+    local       =  e_local.get()
+    descricao   = e_descricao.get()
+    modelo      = e_modelo.get()
+    d_compra    = e_cal.get()
+    v_compra    = e_com.get()
+    serie       = e_serie.get()
+    imagem      = imagem_string
+
+    lista_inserir = [nome, local, descricao, modelo, d_compra, v_compra, serie, imagem]
+    for i in lista_inserir:
+        if i=='':
+            messagebox.showerror('Erro - ','Preencha todos os campos')
+        return
+    inserir_form(lista_inserir)
+    messagebox.showinfo('Sucesso - ','Dados inseridos com sucesso')
+
+    e_nome.delete(0, 'end')
+    e_local.delete(0, 'end')
+    e_descricao.delete(0, 'end')
+    e_modelo.delete(0, 'end')
+    e_cal.delete(0, 'end')
+    e_com.delete(0, 'end')
+    e_serie.delete(0, 'end')
+
+    for widget in framemeio.winfo_children():
+        widget.destroy()
+
+
+
+
+
+
 #INSERIR IMAGEM - ÍCONE
 
 app_img = Image.open('icone.png')
@@ -64,33 +105,33 @@ e_nome.place(x=130,y=11)
 
 l_local = Label(framemeio, text="Sala/Área", height= 1, anchor=NW, font=("Ivy 10 bold"), bg=co1, fg=co4)
 l_local.place(x=10,y=40)
-l_local = Entry(framemeio, width=30, justify='left', relief="solid")
-l_local.place(x=130,y=41)
+e_local = Entry(framemeio, width=30, justify='left', relief="solid")
+e_local.place(x=130,y=41)
 
 l_descricao = Label(framemeio, text="Descrição", height= 1, anchor=NW, font=("Ivy 10 bold"), bg=co1, fg=co4)
 l_descricao.place(x=10,y=70)
-l_descricao = Entry(framemeio, width=30, justify='left', relief="solid")
-l_descricao.place(x=130,y=71)
+e_descricao = Entry(framemeio, width=30, justify='left', relief="solid")
+e_descricao.place(x=130,y=71)
 
 l_modelo = Label(framemeio, text="Marca/Modelo", height= 1, anchor=NW, font=("Ivy 10 bold"), bg=co1, fg=co4)
 l_modelo.place(x=10,y=100)
-l_modelo = Entry(framemeio, width=30, justify='left', relief="solid")
-l_modelo.place(x=130,y=101)
+e_modelo = Entry(framemeio, width=30, justify='left', relief="solid")
+e_modelo.place(x=130,y=101)
 
 l_cal = Label(framemeio, text="Data de Compra", height= 1, anchor=NW, font=("Ivy 10 bold"), bg=co1, fg=co4)
 l_cal.place(x=10,y=130)
-l_cal = DateEntry(framemeio, width=12,bg='darkblue', fg='white', borderwidth=2, year=2022)
-l_cal.place(x=130,y=131)
+e_cal = DateEntry(framemeio, width=12,bg='darkblue', fg='white', borderwidth=2, year=2022)
+e_cal.place(x=130,y=131)
 
 l_com = Label(framemeio, text="Valor da Compra", height= 1, anchor=NW, font=("Ivy 10 bold"), bg=co1, fg=co4)
 l_com.place(x=10,y=160)
-l_com = Entry(framemeio, width=30, justify='left', relief="solid")
-l_com.place(x=130,y=161)
+e_com = Entry(framemeio, width=30, justify='left', relief="solid")
+e_com.place(x=130,y=161)
 
 l_serie = Label(framemeio, text="Número de Série", height= 1, anchor=NW, font=("Ivy 10 bold"), bg=co1, fg=co4)
 l_serie.place(x=10,y=190)
-l_serie = Entry(framemeio, width=30, justify='left', relief="solid")
-l_serie.place(x=130,y=191)
+e_serie = Entry(framemeio, width=30, justify='left', relief="solid")
+e_serie.place(x=130,y=191)
 
 #CRIAÇÃO DE BOTÕES
 
@@ -110,7 +151,7 @@ botao_inserir.place(x=330,y=10)
 #BOTÃO DE ATUALIZAÇÃO
 
 img_update = Image.open('update.png')
-img_update = img_add.resize((20,20))
+img_update = img_update.resize((20,20))
 img_update = ImageTk.PhotoImage(img_update)
 botao_update = Button(framemeio, image=img_update, compound=LEFT, anchor=NW, text="Atualizar".upper(), width=95, overrelief=RIDGE, font=("Ivy 8"), bg=co1, fg=co0)
 botao_update.place(x=330,y=50)
@@ -118,7 +159,7 @@ botao_update.place(x=330,y=50)
 #BOTÃO DE DELETAR
 
 img_delete = Image.open('delete.png')
-img_delete = img_add.resize((20,20))
+img_delete = img_delete.resize((20,20))
 img_delete = ImageTk.PhotoImage(img_delete)
 botao_delete = Button(framemeio, image=img_delete, compound=LEFT, anchor=NW, text="Deletar".upper(), width=95, overrelief=RIDGE, font=("Ivy 8"), bg=co1, fg=co0)
 botao_delete.place(x=330,y=90)
@@ -133,25 +174,22 @@ botao_item.place(x=330,y=221)
 
 #LABELS DE QUANTIDADE TOTAL E VALORES
 
-l_total = Label(framemeio, width=14,height=2,anchor=CENTER,font=('Ivy 17 bold'),bg=co7,fg=co1,relief=FLAT)
-l_total.place(x=450,y=12)
+l_total = Label(framemeio, width=14,height=3,anchor=CENTER,font=('Ivy 17 bold'),bg=co7,fg=co1,relief=FLAT)
+l_total.place(x=450,y=15)
 l_valor_total = Label(framemeio,text="Valor Total de Todos os Itens",anchor=NW,font=('Ivy 10 bold'), bg=co7, fg=co1)
-l_valor_total.place(x=450,y=12)
+l_valor_total.place(x=450,y=15)
 
 l_qtd = Label(framemeio,width=10,height=2,anchor=CENTER,font=('Ivy 25 bold'),bg=co7,fg=co1,relief=FLAT)
-l_qtd.place(x=450,y=92)
+l_qtd.place(x=450,y=110)
 l_qtd_itens = Label(framemeio,text="Quantidade Total de Itens",anchor=NW,font=('Ivy 10 bold'), bg=co7, fg=co1)
-l_qtd_itens.place(x=450,y=92)
+l_qtd_itens.place(x=450,y=114)
 
 #Criando TableView
-def mostrar():
-    tabela_head= ['Item', 'Nome', 'Sala/Área', 'Descrição', 'Marca/Modelo', 'Data da Compra', 'Valor da Compra', 'Número da Série']
+tabela_head= ['Item', 'Nome', 'Sala/Área', 'Descrição', 'Marca/Modelo', 'Data da Compra', 'Valor da Compra', 'Número da Série']
 
-    lista_itens = []
+lista_itens = []
 
-    global tree
-
-    tree = ttk.Treeview(framebaixo, selectmode="extended", columns= tabela_head, show= "headings")
+tree = ttk.Treeview(framebaixo, selectmode="extended", columns= tabela_head, show= "headings")
 
 #Criando Scrollbar Vertical
 
@@ -159,7 +197,7 @@ vsb = ttk.Scrollbar(framebaixo, orient="vertical", command=tree.yview)
 
 #Criando Scrollbar Horizontal
 
-hsb = ttk.Scrollbar(framebaixo, orient="horizontal", command=tree.yview)
+hsb = ttk.Scrollbar(framebaixo, orient="horizontal", command=tree.xview)
 
 #Configurando a Tree
 
@@ -176,10 +214,9 @@ h=[40,150,100,160,130,100,100,100]
 n=0
 
 for col in tabela_head:
-tree.heading(col, text=col.title(), anchor=CENTER)
-tree.column(col, width=h[n], anchor=hd[n])
-
-n+=1
+    tree.heading(col, text=col.title(), anchor=CENTER)
+    tree.column(col, width=h[n], anchor=hd[n])
+    n+=1
 
 for item in lista_itens:
     tree.insert('','end', values=item)
@@ -192,8 +229,7 @@ Total_valor = sum(quantidade)
 Total_itens = sum(quantidade)
 
 l_total['text'] = 'R$ {:,.2f}'.format(Total_valor)
-l_qtd[]
+l_qtd['text'] = Total_itens
 
-mostrar()
 
 janela.mainloop()
